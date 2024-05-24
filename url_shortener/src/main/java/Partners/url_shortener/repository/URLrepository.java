7a73c4bca07f188ea36cf3a5af7ed355c0d1527e
yjun0915 <kyjun09150916@gmail.com>
2024-05-24 18:47:12 +0900

    [feature]: findByOriginalURL

--- url_shortener/src/main/java/Partners/url_shortener/repository/URLrepository.java
+++ url_shortener/src/main/java/Partners/url_shortener/repository/URLrepository.java
@@ -3,5 +3,9 @@ package Partners.url_shortener.repository;
 import Partners.url_shortener.domain.URL;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.lang.reflect.Member;
+import java.util.Optional;
+
 public interface URLrepository extends JpaRepository<URL, Long> {
+    Optional<URL> findByOriginalURL(String originalURL);
 }
