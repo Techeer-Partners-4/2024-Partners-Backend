80bf6dbd26b59db51f9f5ad695dead3762dca0d2
yjun0915 <kyjun09150916@gmail.com>
2024-05-24 18:43:11 +0900

    [feature]: recreate getter and setter

--- url_shortener/src/main/java/Partners/url_shortener/domain/URL.java
+++ url_shortener/src/main/java/Partners/url_shortener/domain/URL.java
@@ -20,15 +20,43 @@ public class URL {
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public Long getId(){
+    public Long getId() {
         return id;
     }
 
-    public void setId(Long id){
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getOriginalURL(){
+    public String getOriginalURL() {
         return originalURL;
+    }
+
+    public void setOriginalURL(String originalURL) {
+        this.originalURL = originalURL;
+    }
+
+    public String getShortenURL() {
+        return shortenURL;
+    }
+
+    public void setShortenURL(String shortenURL) {
+        this.shortenURL = shortenURL;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
