8d809e87ac8daecd82d22d08327d583f2c7f40ce
yjun0915 <kyjun09150916@gmail.com>
2024-05-24 18:17:12 +0900

    [document]: URL model

---
+++ url_shortener/src/main/java/Partners/url_shortener/domain/URL.java
@@ -0,0 +1,24 @@
+package Partners.url_shortener.domain;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
+@Entity
+public class URL {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String originalURL;
+
+    private String shortenURL;
+
+    private String hash;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+}
