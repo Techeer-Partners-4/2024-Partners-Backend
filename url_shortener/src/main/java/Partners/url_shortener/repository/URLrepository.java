bd5e1dbee65401835dbdc4c926556f5b65342f86
yjun0915 <kyjun09150916@gmail.com>
2024-05-25 15:22:54 +0900

    [refactor]: delete unused import

--- url_shortener/src/main/java/Partners/url_shortener/repository/URLrepository.java
+++ url_shortener/src/main/java/Partners/url_shortener/repository/URLrepository.java
@@ -3,7 +3,6 @@ package Partners.url_shortener.repository;
 import Partners.url_shortener.domain.URL;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.reflect.Member;
 import java.util.Optional;
 
 public interface URLrepository extends JpaRepository<URL, Long> {
