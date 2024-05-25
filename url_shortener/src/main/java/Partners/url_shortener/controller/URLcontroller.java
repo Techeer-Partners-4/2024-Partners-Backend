dabcfbe9a513bd306143ac86c3a0e0d95edeffa9
yjun0915 <kyjun09150916@gmail.com>
2024-05-25 15:57:51 +0900

    [feature]: redirect to original url

--- url_shortener/src/main/java/Partners/url_shortener/controller/URLcontroller.java
+++ url_shortener/src/main/java/Partners/url_shortener/controller/URLcontroller.java
@@ -1,8 +1,34 @@
 package Partners.url_shortener.controller;
 
+import Partners.url_shortener.domain.URL;
+import Partners.url_shortener.service.URLservice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class URLcontroller {
 
+    @Autowired
+    private URLservice ureService;
+
+    @GetMapping("/")
+    public String noboarding() {
+        return "onboarding";
+    }
+
+    @GetMapping("/{shortenURL}")
+    public String redirectiongURL(@PathVariable String shortenURL, Model model){
+        Optional<URL> url = URLservice.findByShortenUrl(shortenURL);
+        if (url.isPresent()) {
+            return "redircet:" + url.get().getOriginalURL();
+        } else {
+            model.addAttribute("error", "URL not found");
+            return "error";
+        }
+    }
 }
