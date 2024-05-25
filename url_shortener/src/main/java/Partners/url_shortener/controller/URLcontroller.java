57c0b430ce5c74774b5d0cb088c6e94763c61e0a
yjun0915 <kyjun09150916@gmail.com>
2024-05-25 16:18:30 +0900

    [feature]: postmapping result

--- url_shortener/src/main/java/Partners/url_shortener/controller/URLcontroller.java
+++ url_shortener/src/main/java/Partners/url_shortener/controller/URLcontroller.java
@@ -5,8 +5,7 @@ import Partners.url_shortener.service.URLservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -19,6 +18,13 @@ public class URLcontroller {
     @GetMapping("/")
     public String noboarding() {
         return "onboarding";
+    }
+
+    @PostMapping("/shorten")
+    public String shortenUrl(@RequestParam("url") String originalUrl, Model model) {
+        URL url = URLservice.shortenURL(originalUrl);
+        model.addAttribute("shortUrl", url.getShortenURL());
+        return "result";
     }
 
     @GetMapping("/{shortenURL}")
